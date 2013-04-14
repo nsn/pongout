6fa1f94ebe8604f1cd69810823944cda40d6363d
nsn <nsn@nightspawn.com>
2013-04-14 15:17:44 +0200

    deployment

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 OUTDIR=package
 DESTDIR=$OUTDIR/pongout
+ZIPFILE=pongout.zip
 FILE=$(find java/target/  -name pongout\*.jar |head -n1)
 BASE=$(basename $FILE)
 JAVACMD="java -Djava.library.path=./natives -jar $BASE"
@@ -12,5 +13,13 @@ cp -r java/target/natives $DESTDIR
 echo "#!/bin/sh\n${JAVACMD}" > $DESTDIR/pongout.sh
 echo "${JAVACMD}" > $DESTDIR/pongout.bat
 
-cd $OUTDIR && zip -r  pongout.zip pongout
+cd $OUTDIR 
+zip -r ${ZIPFILE} pongout
+cd ..
+
+REMOTE=u69386259@nightspawn.com:satansoft.de/pongout
+read -p "upload to ${REMOTE}? [y/N]" REPLY
+[ "$REPLY" != "y" ] && exit 1
+chmod ugo+r $OUTDIR/${ZIPFILE}
+scp ./$OUTDIR/${ZIPFILE} ${REMOTE}
 
