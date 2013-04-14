220ede3369c69abc38623036a366b8ee67f95b75
nsn <nsn@nightspawn.com>
2013-04-14 14:04:04 +0200

    package sh

--- package.sh
+++ package.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 OUTDIR=package
 DESTDIR=$OUTDIR/pongout
-FILE=$(find java/target/ -name pongout\*.jar |head -n1)
-JAVACMD="java -Djava.library.path=./natives -jar $FILE"
+FILE=$(find java/target/  -name pongout\*.jar |head -n1)
+BASE=$(basename $FILE)
+JAVACMD="java -Djava.library.path=./natives -jar $BASE"
 rm -rf $OUTDIR
 mkdir -p $DESTDIR
 cp $FILE $DESTDIR
